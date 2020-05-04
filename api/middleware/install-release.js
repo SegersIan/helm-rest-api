7b50b44c8f6aed3736d0d3ce0b1a709075cafd8e
SegersIan <iansegers@gmail.com>
2020-05-04 04:47:33 +0200

    Use --output json parameter

--- api/middleware/install-release.js
+++ api/middleware/install-release.js
@@ -3,7 +3,9 @@ const { run } = require('../helm');
 module.exports = async (req, res) => {
     try {
         res.setHeader('Content-Type', 'text/plain');
-        const versionInfo = await run('version');
+        const releaseName = "";
+        const chart = "";
+        const versionInfo = await run(`install --output json --atomic ${releaseName} ${chart}`);
         res.send(versionInfo.stdOut);
     } catch (error) {
         console.error(error);
