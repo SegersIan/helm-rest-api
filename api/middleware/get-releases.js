875a0bb8700d4e6cb8194237bfee8d11768d5da4
SegersIan <iansegers@gmail.com>
2020-05-28 10:57:19 +0200

    Test

--- api/middleware/get-releases.js
+++ api/middleware/get-releases.js
@@ -1,13 +1,21 @@
-const { run } = require('../helm');
+const {run} = require('../helm');
 
 module.exports = async (req, res) => {
     try {
         const output = await run('list --output json');
         const releases = JSON.parse(output.stdOut);
         res.setHeader('Content-Type', 'text/plain');
-        res.send(JSON.stringify(releases, null, 4));
+        //res.send(JSON.stringify(releases, null, 4));
+
+        const kc = await getKubeConfig();
+        res.send(kc);
+
     } catch (error) {
         console.error(error);
         res.send(error.toString());
     }
+}
+
+async function getKubeConfig() {
+    return "";
 }
