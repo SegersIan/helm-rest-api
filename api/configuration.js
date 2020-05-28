2f5d6826accbfcaa070f636412ba24aff73adb31
SegersIan <iansegers@gmail.com>
2020-05-28 11:50:56 +0200

    Fixes

--- api/configuration.js
+++ api/configuration.js
@@ -2,5 +2,10 @@ module.exports = {
     name: "HELM REST API",
     server : {
         port : 3000
+    },
+    aks: {
+        subscriptionId : process.env.AKS_SUBSCRIPTION_ID,
+        resourceGroup : process.env.AKS_RESOURCE_GROUP,
+        name : process.env.AKS_NAME,
     }
 }
--- api/middleware/get-releases.js
+++ api/middleware/get-releases.js
@@ -1,13 +1,16 @@
+const request = require('request-promise-native');
+
+const config = require('../configuration');
 const {run} = require('../helm');
 
 module.exports = async (req, res) => {
     try {
-        const output = await run('list --output json');
-        const releases = JSON.parse(output.stdOut);
-        res.setHeader('Content-Type', 'text/plain');
+        //const output = await run('list --output json');
+        //const releases = JSON.parse(output.stdOut);
         //res.send(JSON.stringify(releases, null, 4));
 
         const kc = await getKubeConfig();
+        res.setHeader('Content-Type', 'text/plain');
         res.send(kc);
 
     } catch (error) {
@@ -17,5 +20,37 @@ module.exports = async (req, res) => {
 }
 
 async function getKubeConfig() {
-    return "";
+    const token = await getManagedIdentity();
+    console.log(token);
+    return getAdminCredentials(token);
+}
+
+async function getManagedIdentity(resource = "https://management.azure.com") {
+    const response = await request({
+        method: "get",
+        url: "http://169.254.169.254/metadata/identity/oauth2/token",
+        qs: {
+            "api-version": "2018-02-01",
+            "resource": resource
+        },
+        headers: {
+            'Metadata': 'true'
+        }
+    })
+    return JSON.parse(response);
+}
+
+async function getAdminCredentials({access_token, token_type}) {
+    const response = await request({
+        method: "post",
+        url: `https://management.azure.com/subscriptions/${config.aks.subscriptionId}/resourceGroups/${config.aks.resourceGroup}/providers/Microsoft.ContainerService/managedClusters/${config.aks.name}/listClusterAdminCredential`,
+        qs: {
+            "api-version": "2020-03-01"
+        },
+        headers: {
+            "Authorization": `${token_type} ${access_token}`,
+            "Content-type": "application/json"
+        }
+    })
+    return JSON.parse(response);
 }
